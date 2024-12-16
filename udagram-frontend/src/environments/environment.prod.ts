5f3ad9fd2a1a185a479d3db82d780c4541cab635
Le Dieu <hongledt1905@gmail.com>
2024-12-16 10:04:58 +0700

    update code

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "http://a8eb4d968d48b466ca765175275c9ba4-1026182285.us-east-1.elb.amazonaws.com:8080/api/v0",
 };
 
 /*
